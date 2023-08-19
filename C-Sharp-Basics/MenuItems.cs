fc65b1485faa33eb5be897b6856fa08cf78ebd97
JuanDAC <juanavila12.0308@gmail.com>
2023-08-19 12:52:12 -0500

    []()

--- C-Sharp-Basics/MenuItems.cs
+++ C-Sharp-Basics/MenuItems.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace C_Sharp_Basics
 {
+    internal delegate void Process();
+
     internal class MenuItems
     {
-
-    }
+        public Process process;
+        public string message;
+        public MenuItems(Process process, string message) {
+            this.message = message;
+            this.process = process;
+        }
+    };
 }
--- C-Sharp-Basics/Program.cs
+++ C-Sharp-Basics/Program.cs
@@ -2,15 +2,86 @@
 {
     internal class Program
     {
+        static MenuItems[] items = new MenuItems[] {
+            new MenuItems(
+                delegate () {
+                    try {
+                        Console.WriteLine("Input a number that you whan to add 1:");
+                        Console.WriteLine(Int32.Parse(Console.ReadLine()) + 1);
+                    } catch {
+                        Console.WriteLine("Try to input a valid format");
+                    }
+                },
+               "Next Number"
+            ),
+            new MenuItems(
+                delegate () {
+                    try {
+                        Double number = (Double)(new Random()).Next(1, 200);
+                        Console.WriteLine($"Your random number is: {number}");
+                        Console.WriteLine($"if we add 30% the result is: {number + number*0.3}");
+                    } catch {
+                        Console.WriteLine("Try to input a valid format");
+                    }
+                },
+                "Add 30 percentage to random number"
+            ),
+            new MenuItems(
+                delegate () {
+                    try {
+                        Console.WriteLine("Input the height of the rectangle:");
+                        Double height = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Input the width of the rectangle:");
+                        Double width = Double.Parse(Console.ReadLine());
+                        Console.WriteLine($"The area of the rectangle is: {height * width}");
+                        Console.WriteLine($"The perimeter of the rectangle is: {height * 2 + width * 2}");
+                    } catch {
+                        Console.WriteLine("Try to input a valid format");
+                    }
+                },
+                "Calculate the perimeter and area"
+            ),
+            new MenuItems(
+                delegate () {
+                    Environment.Exit(0);
+                },
+                "Close the program"
+            )
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Menu();
+                Selector();
+            }
+        }
 
-            MenuItems items = new MenuItems();
+        static void Menu()
+        {
+            Console.WriteLine("Select a option to execute:");
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {items[i].message}");
+            }
 
         }
+
+        static void Selector()
+        {
+            Console.WriteLine("Input your option here:");
+            string input = Console.ReadLine();
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (Int32.Parse(input.Trim()) == (i + 1))
+                {
+                    Console.WriteLine($"Your selection as {i + 1}. {items[i].message}");
+                    items[i].process();
+                    Console.WriteLine("Press any number to continue");
+                    Console.ReadLine();
+                }
+            }
+        }
     }
-
-
-
 }
