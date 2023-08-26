5226490778580dfd2a5756c95226125ae0317b71
JuanDAC <juanavila12.0308@gmail.com>
2023-08-26 13:19:58 -0500

    [:fire:] first UI (my fisth UI in razor)

---
+++ UI_activities/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI_activities.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
